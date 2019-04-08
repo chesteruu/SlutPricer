415d84c5e61985d1289d092a86d9f0119ea12ae6
Gang Xu <chester.uu@gmail.com>
2019-04-08 16:08:07 +0200

    Add Booli

--- HemnetSlutPricer/Program.cs
+++ HemnetSlutPricer/Program.cs
@@ -12,49 +12,72 @@ namespace HemnetSlutPricer
         {
             if(args.Count() == 0)
             {
-                Console.WriteLine("pricer location stockholm.");
+                Console.WriteLine("pricer location <hemnet|booli> <area>");
                 Console.WriteLine("pricer get <location_id> <type> <timespan> <filepath>");
                 return;
             }
 
-            if(args.Count() == 2)
+            if(args.Count() == 3)
             {
-                var result = HemnetPricer.GetAreaId(args[1]);
+                if(args[1] == "hemnet")
+                {
+                    var result = HemnetPricer.GetAreaId(args[2]);
 
-                result.Wait();
-                var locations = result.Result;
-                Console.WriteLine(String.Join("\n", locations.Select(x => x.id.ToString() + "\t" + x.name + "\t\t" + x.parent_location.name).ToArray()));
-                return;
+                    result.Wait();
+                    var locations = result.Result;
+                    Console.WriteLine(String.Join("\n", locations.Select(x => x.id.ToString() + "\t" + x.name + "\t\t" + x.parent_location.name).ToArray()));
+                    return;
+                }
+                
+                if(args[1] == "booli")
+                {
+                    var result = BooliPricer.GetAreaId(args[2]);
+
+                    result.Wait();
+                    var locations = result.Result;
+                    Console.WriteLine(String.Join("\n", locations.Select(x => x.area.id.ToString() + "\t" + x.area.name + "\t" + x.area.url + "\t\t" + x.label).ToArray()));
+                    return;
+                }
             }
 
-            if (args.Count() == 5)
+            if (args.Count() == 6)
             {
                 List<string> locationIds = args[1].Split(";").ToList();
                 List<string> type = args[2].Split(";").ToList();
-           
-                PriceScrapper.HemnetPricer hemnetPricer = new HemnetPricer(locationIds, type, args[3]);
-                hemnetPricer.DoRequest().Wait();
 
-                var priceInfoList = hemnetPricer.GetPriceInfoList();
+                if (args[5] == "hemnet")
+                {
+                    PriceScrapper.HemnetPricer hemnetPricer = new HemnetPricer(locationIds, type, args[3]);
+                    hemnetPricer.DoRequest().Wait();
 
-                using (StreamWriter sw = new StreamWriter(args[4]))
+                    var priceInfoList = hemnetPricer.GetPriceInfoList();
+
+                    using (StreamWriter sw = new StreamWriter(args[4]))
+                    {
+
+                        sw.WriteLine("Total Object: " + priceInfoList.Count);
+                        Console.WriteLine("Total Object: " + priceInfoList.Count);
+
+                        if (priceInfoList.Count > 0)
+                        {
+                            sw.WriteLine("Searched Time: " + priceInfoList[0].SoldTime.ToShortDateString() + " => " + priceInfoList[priceInfoList.Count - 1].SoldTime.ToShortDateString());
+                            Console.WriteLine("Searched Time: " + priceInfoList[0].SoldTime.ToShortDateString() + " => " + priceInfoList[priceInfoList.Count - 1].SoldTime.ToShortDateString());
+                        }
+
+                        foreach (var priceInfo in hemnetPricer.GetPriceInfoList())
+                        {
+                            sw.WriteLine(priceInfo.ToJson());
+                            Console.WriteLine(priceInfo.ToString());
+                        }
+
+                    }
+                }
+
+                if(args[5] == "booli")
                 {
 
-                    sw.WriteLine("Total Object: " + priceInfoList.Count);
-                    Console.WriteLine("Total Object: " + priceInfoList.Count);
-
-                    if (priceInfoList.Count > 0)
-                    {
-                        sw.WriteLine("Searched Time: " + priceInfoList[0].SoldTime.ToShortDateString() + " => " + priceInfoList[priceInfoList.Count - 1].SoldTime.ToShortDateString());
-                        Console.WriteLine("Searched Time: " + priceInfoList[0].SoldTime.ToShortDateString() + " => " + priceInfoList[priceInfoList.Count - 1].SoldTime.ToShortDateString());
-                    }
-
-                    foreach (var priceInfo in hemnetPricer.GetPriceInfoList())
-                    {
-                        sw.WriteLine(priceInfo.ToJson());
-                        Console.WriteLine(priceInfo.ToString());
-                    }
-
+                    BooliPricer booliPricer = new BooliPricer(locationIds, type, args[3]);
+                    booliPricer.DoRequest().Wait();
                 }
             }
         }
---
+++ PriceScrapper/BooliPricer.cs
@@ -0,0 +1,128 @@
+﻿using HtmlAgilityPack;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace PriceScrapper
+{
+    public class BooliArea
+    {
+        public string name;
+        public Int64 id;
+        public string type;
+        public string url;
+    }
+    public class BooliLocations
+    {
+        public BooliArea area;
+        public string label;
+        public string type;
+        public override string ToString()
+        {
+            return JsonConvert.SerializeObject(this);
+        }
+    }
+    public class BooliPricer
+    {
+        private const string BOOLI_PREFIX = "https://www.booli.se/slutpriser/";
+        private const string BOOLI_AREA_ID_PREFIX = "https://www.booli.se/search/suggestions?q=";
+
+        private List<string> m_locationIds;
+        private List<string> m_itemTypes;
+        private string m_timeSpan;
+
+        public BooliPricer(List<string> locationId, List<string> itemTypes, string timeSpan)
+        {
+            m_locationIds = locationId;
+            m_itemTypes = itemTypes;
+            m_timeSpan = timeSpan;
+        }
+
+        public static async Task<BooliLocations[]> GetAreaId(string query)
+        {
+            query = HttpUtility.UrlEncode(query);
+            HttpClient client = new HttpClient();
+            string url = BOOLI_AREA_ID_PREFIX + query;
+            using (var response = await client.GetAsync(url))
+            {
+                using (var content = response.Content)
+                {
+                    var result = await content.ReadAsStringAsync();
+                    BooliLocations[] booliLocations = JsonConvert.DeserializeObject<BooliLocations[]>(result);
+                    return booliLocations;
+                }
+            }
+        }
+
+        public async Task DoRequest()
+        {
+            if (m_timeSpan.EndsWith("m"))
+            {
+                return;
+            }
+
+            int month = 0;
+            int.TryParse(m_timeSpan.Substring(0, m_timeSpan.IndexOf('m')), out month);
+
+            if (month == 0)
+            {
+                throw new Exception("Wrong time, it should be <number>m, e.g. 36m");
+            }
+
+            string locationIdsQuery = "/" + String.Join(",", m_locationIds.ToArray()) +"/";
+            string itemTypesQuery = "&objectType=" + String.Join(",", m_itemTypes.ToArray());
+            string minSoldDate = "&minSoldDate=" + DateTime.Now.AddMonths(-month).ToShortDateString();
+            string maxSoldDate = "&maxSoldDate=" + DateTime.Now.ToShortDateString();
+            string pageQuery = "&page=";
+            int pageNum = 1;
+
+            string fullQuery = BOOLI_PREFIX + locationIdsQuery + itemTypesQuery + minSoldDate + maxSoldDate;
+            HttpClient client = new HttpClient();
+
+            while (true)
+            {
+                string url = fullQuery + pageQuery + pageNum;
+                using (var response = await client.GetAsync(url))
+                {
+                    using (var content = response.Content)
+                    {
+                        // read answer in non-blocking way
+                        var result = await content.ReadAsStringAsync();
+                        var document = new HtmlDocument();
+                        document.LoadHtml(result);
+                        var resultNodes = document.DocumentNode.SelectNodes("//li[@class='search-list__item']/a");
+
+                        if (resultNodes == null)
+                        {
+                            break;
+                        }
+
+                        pageNum++;
+
+                        foreach (var node in resultNodes)
+                        {
+                            PriceInfo priceInfo = new PriceInfo();
+                            foreach (var property in node.ChildNodes)
+                            {
+                                if (!property.HasAttributes)
+                                {
+                                    continue;
+                                }
+
+                                switch (property.Attributes["class"].Value)
+                                {
+                                    
+                                }
+                            }
+                        }
+
+                    }
+                }
+            }
+        }
+    }
+}
